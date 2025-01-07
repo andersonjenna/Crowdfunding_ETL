9af07aeef6ec54b87cfa68d8eb85d3bc46416c90
ander@Jenna <andersonjenna01@gmail.com>
2025-01-06 20:12:02 -0600

    saving sql file

--- crowdfunding_db_schema.sql.sql
+++ crowdfunding_db_schema.sql.sql
@@ -3,7 +3,7 @@
 
 
 CREATE TABLE "category" (
-    "category_id" INT   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
     "category" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
@@ -11,7 +11,7 @@ CREATE TABLE "category" (
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" INT   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
     "subcategory" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
@@ -19,7 +19,7 @@ CREATE TABLE "subcategory" (
 );
 
 CREATE TABLE "contacts" (
-    "contact_id" INT   NOT NULL,
+    "contact_id" VARCHAR   NOT NULL,
     "first_name" VARCHAR(100)   NOT NULL,
     "last_name" VARCHAR(100)   NOT NULL,
     "email" VARCHAR(255)   NOT NULL,
@@ -29,22 +29,26 @@ CREATE TABLE "contacts" (
 );
 
 CREATE TABLE "campaign" (
-    "cf_id" INT   NOT NULL,
-    "contact_id" INT   NOT NULL,
+    "cf_id" VARCHAR   NOT NULL,
+    "contact_id" VARCHAR   NOT NULL,
     "company_name" VARCHAR(255)   NOT NULL,
     "description" TEXT   NOT NULL,
     "goal" FLOAT   NOT NULL,
     "pledged" FLOAT   NOT NULL,
     "outcome" VARCHAR(50)   NOT NULL,
-    "backers_count" INT   NOT NULL,
+    "backers_count" VARCHAR   NOT NULL,
     "country" VARCHAR(100)   NOT NULL,
     "currency" VARCHAR(50)   NOT NULL,
-    "launch_date" TIMESTAMP   NOT NULL,
-    "end_date" TIMESTAMP   NOT NULL,
-    "category_id" INT   NOT NULL,
-    "subcategory_id" INT   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
+SELECT * FROM category LIMIT 10;
+SELECT * FROM subcategory LIMIT 10;
+SELECT * FROM contacts LIMIT 10;
+SELECT * FROM campaign LIMIT 10;
