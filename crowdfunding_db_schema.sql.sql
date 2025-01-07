c2e5f0227daa335fc15ca854ef5010116f0c3b7c
ander@Jenna <andersonjenna01@gmail.com>
2025-01-06 19:22:45 -0600

    adding sql file

---
+++ crowdfunding_db_schema.sql.sql
@@ -0,0 +1,50 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "category" (
+    "category_id" INT   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" INT   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(100)   NOT NULL,
+    "currency" VARCHAR(50)   NOT NULL,
+    "launch_date" TIMESTAMP   NOT NULL,
+    "end_date" TIMESTAMP   NOT NULL,
+    "category_id" INT   NOT NULL,
+    "subcategory_id" INT   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
